4b250cba79fd9d5025432b18facea072ee0d4261
Tom Tremerel <156415815+TomTremerel@users.noreply.github.com>
2025-04-15 15:14:36 +0200

    Add files via upload

---
+++ quciksort.py
@@ -0,0 +1,16 @@
+P = [1,23,34,3,5,3,1,45,3]
+
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quicksort(left) + middle + quicksort(right)
+
+
+
+
+sorted_list = quicksort(P)
+print(sorted_list)
